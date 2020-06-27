f86cbfe3daf4b944faee03ffdc646df482b09331
Felipe J. R. Vieira <felipejrvieira@gmail.com>
2020-06-27 19:07:24 -0300

    Criação de Controller para gerenciar negociação e lista de negociação.

---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,18 @@
+class DateHelper{
+
+    constructor(){
+        throw new Error("DateHelper não pode ser instanciado");
+    }
+    static textoParaData(texto){
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error("Data no formato inválido!");
+        return new Date(
+            ...texto
+              .split("-")
+              .map((item, indice) => item - (indice % 2))
+          );
+    }
+
+    static dataParaTexto(data){
+        return '${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}';
+    }
+}
